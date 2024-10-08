4dc3c61a75b3c814f58dfc549e4d171b981ace71
alexxhub <alexxhub@mail.ru>
2024-10-08 11:11:56 +0300

    sql

--- lesson_sql/index.sql
+++ lesson_sql/index.sql
@@ -7,3 +7,35 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 DROP TABLE IF EXISTS users;
+
+/* индексы */
+CREATE INDEX user_id ON users (ID);
+CREATE UNIQUE INDEX title ON posts (TITLE), blogs (TITLE);
+DROP INDEX user_id ON users;
+
+/* CRUD
+c - create
+r - read
+u - update
+d - delete
+ */
+
+/* C. Добавление элементов в таблицу БД */
+INSERT INTO users (LOGIN, PASSWORD, NAME, DATE_REGISTRATION) VALUES ('admin', '123456', 'Petia', CURRENT_DATE);
+
+/* R. Чтение из таблицы БД */
+SELECT * FROM users;
+SELECT LOGIN, NAME, AGE FROM users;
+SELECT LOGIN, NAME, AGE FROM users WHERE DATE_REGISTRATION < CURRENT_DATE - '100 day';
+SELECT AGE * 365 AS DAYS, AGE FROM users;
+SELECT * FROM users WHERE (DATE_REGISTRATION < DATE('02.03.24') AND NAME != 'Petia')
+                            OR AGE > 12 XOR AGE < 40;
+SELECT * FROM users WHERE AGE BETWEEN(12, 40);
+
+SELECT * FROM users, groups WHERE users.GROUP_ID = groups.ID RIGHT JOIN rules WHERE rules.ID = groups.RULE_ID;
+
+/* U. Обновление данных в таблицах БД */
+UPDATE users SET LOGIN = 'admin2', PASSWORD = '2344567' WHERE ID=1;
+
+/* D. Удаление записи из таблицы БД */
+DELETE FROM users WHERE ID = 10;
